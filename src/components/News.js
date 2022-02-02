7394026e1ba5a19f70ce145da0a96633d0b531a0
ishankrsingh <ishankrsingh@gmail.com>
2022-02-02 11:38:21 +0530

    3 - Fetching API Key from News API

--- src/components/News.js
+++ src/components/News.js
@@ -6,14 +6,21 @@ import NewsItem from './NewsItem';
 export class News extends Component {
   render() {
     return (
-    <div>
-        This is news component.
-        <NewsItem></NewsItem>
-        <NewsItem></NewsItem>
-        <NewsItem></NewsItem>
-        <NewsItem></NewsItem>
-        <NewsItem></NewsItem>
-        <NewsItem></NewsItem>
+    <div className="container my-3">
+        <h2>Top Headlines</h2>
+        <div className="row">
+            {/* In bootstrap there is grid of 12 columns. col-md-4 means that in medium devices width of one column will be equal to 4 columns. Therefore we will have 3 such columns of width of 4 columns(original bootstrap grid). Also we have given dummy values to title and description below for the time being.*/}
+            <div className="col-md-4">
+            <NewsItem title="Title" description="Desc"></NewsItem> 
+            </div>
+            <div className="col-md-4">
+            <NewsItem title="Title" description="Desc"></NewsItem>
+            </div>
+            <div className="col-md-4">
+            <NewsItem title="Title" description="Desc"></NewsItem>
+            </div>
+        </div>
+        
     </div>);
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,22 @@
 // rce -> React's class based component
+import { getByTitle } from '@testing-library/react';
 import React, { Component } from 'react';
 
 export class NewsItem extends Component {
   render() {
+      let {title,description} = this.props; //Destructuring in JS
     return (
     <div>
-        This is a News item.
+        {/* Card from Bootstrap */}
+        <div className="card" style={{width:"18rem"}}> 
+        {/* Since style attribute above,was inline by default(refer its code in bootstrap doc) therfore we have converted it into an object*/}
+          <img src="https://cdn.cnn.com/cnnnext/dam/assets/220201180534-tonga-jan-27-super-tease.jpg" className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+            </div>
+</div>
     </div>);
   }
 }
