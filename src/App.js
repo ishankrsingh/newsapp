daff23fe07214c379098e8df434092572c78d14d
ishankrsingh <ishankrsingh@gmail.com>
2022-02-04 09:09:41 +0530

    10 - Fetching News category-wise from API using router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import News from './components/News';
 //rcc -> This gives us class based react component below because of ES7 React/ Redux/ GraphQL extension
 import React, { Component } from 'react';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 export default class App extends Component {
 
   // d ='ishan'; // class variable
@@ -14,16 +20,39 @@ export default class App extends Component {
   render() {
     return ( //We have used () here for returning data as our code is longer than a single line.
     <div>
+      <Router>
       <NavBar>
 
       </NavBar>
       {/* Through the pageSize prop below, we will be able to limit the no. of news items we want to see in a single page*/}
-      <News pageSize={9} country="in" category="technology"> 
+      
 
-      </News>
-         {/* Hello {this.d}, this is my first class based component.  */}
-          {/* As we can see in the above example, we can only access class variables using this operator */}
+      
+
+       {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL.   Below is the updated code of React Router Version 6*/}
+            {/* Note: Prior to adding unique key to every News component below, even when we were clicking on any of the categories,although path in the url was changing but page was not re-rendering or remounting. If we wanted to look at the new results, reload was required to re-render the page. This was happening because we are using News component in every Route, so react was not re-rendering or remounting. So we had to add key(should be unique) to every News component in order to make it unique and thus telling react to re-render the page once we click on any of the categories. */}
+        <Routes>
+          <Route path="/"  element={ <News key="general" pageSize={9} country="in" category="general"/>}/>
+          <Route path="/business"  element={ <News key="business" pageSize={9} country="in" category="business"/>}/>
+          <Route path="/entertainment"  element={ <News key="entertainment" pageSize={9} country="in" category="entertainment"/>}/>
+          <Route path="/health"  element={ <News key="health" pageSize={9} country="in" category="health"/>}/>
+          <Route path="/science"  element={ <News key="science" pageSize={9} country="in" category="science"/>}/>
+          <Route path="/sports"  element={ <News key="sports" pageSize={9} country="in" category="sports"/>}/>
+          <Route path="/technology"  element={ <News key="technology" pageSize={9} country="in" category="technology"/>}/>
+          
+        </Routes>
+      </Router>
     </div>
     )
   }
 }
+
+// React router version below 6 -> Whatever is wriiten between <Route></Route> will work if the url of its path matches.Whatever is wriiten between <Route></Route> will work if the url of its path matches.
+//<Route path="/"><News pageSize={9} country="in" category="general"/> </Route>
+//           <Route path="/business"><News pageSize={9} country="in" category="business"/> </Route>
+//           <Route path="/entertainment"><News pageSize={9} country="in" category="entertainment"/> </Route>
+//           <Route path="/health"><News pageSize={9} country="in" category="health"/> </Route>
+//           <Route path="/science"><News pageSize={9} country="in" category="science"/> </Route>
+//           <Route path="/sports"><News pageSize={9} country="in" category="sports"/> </Route>
+//           <Route path="/technology"><News pageSize={9} country="in" category="technology"/> </Route>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
+import {Link} from "react-router-dom"; //Since we are only using Link here, we are just importing that
+
 export class NavBar extends Component {
   static propTypes = {};
 
@@ -13,36 +15,37 @@ export class NavBar extends Component {
         {/* Then replace all class= to className= , Also look for closing tags where its not already present like hr and input in this case*/}
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">NewsMonkey</a>
+    <Link className="navbar-brand" to="/">NewsMonkey</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" href="/about">Business</a>
+          <Link className="nav-link active" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" href="/about">Entertainment</a>
+          <Link className="nav-link active" to="/business">Business</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" href="/about">General</a>
+          <Link className="nav-link active" to="/entertainment">Entertainment</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" href="/about">Health</a>
+          <Link className="nav-link active" to="/health">Health</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" href="/about">Science</a>
+          <Link className="nav-link active" to="science">Science</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" href="/about">Sports</a>
+          <Link className="nav-link active" to="/sports">Sports</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" href="/about">Technology</a>
+          <Link className="nav-link active" to="/technology">Technology</Link>
         </li>
-        <li className="nav-item">
+        {/* Note: We have replaced all the a tag with Link and href with to, because if we use a instead of Link it will reload the page every single time. */}
+        {/* <li className="nav-item">
           <a className="nav-link active" href="/about">About</a>
-        </li>
+        </li> */}
       </ul>
       
     </div>
